dabce64205899353fc2f336a0e0fa3a732fd9e82
OlamideShogbamu <shogbamuolamide@gmail.com>
2024-11-25 16:13:46 +0100

    added CORS to flask app

--- mask.py
+++ mask.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from dotenv import load_dotenv
 from groq import Groq
 
@@ -6,6 +7,8 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
+
+CORS(app)
 
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 # This is where your cached data will be stored
